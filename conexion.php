b0638355d67bd96f1b90f2b295072f8b77b6aff0
Tristain <emanuel.97tristain@gmail.com>
2019-08-01 09:19:35 -0500

    Ignorar

--- conexion.php
+++
@@ -1,8 +0,0 @@
-<?php
-    $conexion = mysqli_connect( 'localhost', 'root', '', 'proyecto'); //Conecta a la base de datos
-    if(mysqli_connect_errno()){//En caso de que no se logre conectar
-        echo '<p>Error: no se logró conectar a la Base de Datos.<br />
-        Por favor intenta de nuevo más tarde.</p>';
-        exit();
-    }
-?>
--- validaruno.php
+++
@@ -1,7 +0,0 @@
-<?php
-    session_start();
-    if(!isset($_SESSION["user"]) || $_SESSION['nivel']!=1){
-        header("Location:inicio.html");
-        exit();
-    }
-?>
